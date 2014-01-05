850a69fa08a9a1c3a30df52e501012cb724c1d19
John Mee <github.com@johnmee.com>
2014-01-05 22:34:20 +1100

    Refactor as a presentation agnostic chart, and add tallys for each colour.

--- txt_lotto.py
+++ txt_lotto.py
@@ -11,9 +11,23 @@ WEEK_FILENAME = 'MondayWednesdayLotto.csv'
 
 # number of balls in a draw
 MAX_BALLS = 45
+BALLS = range(1, MAX_BALLS+1)
 
 # discard any draws older than this
 OLDEST_DRAW = datetime.date.today() - datetime.timedelta(weeks=104)
+
+LOTTO_NAME_MAP = ('Monday', 'OzLotto', 'Wednesday', 'Thu', 'Fri', 'TattsLotto', 'Sun')
+TALLY_NAMES = ('Green', 'Gold', 'Blue', 'Pink', 'Drawn', 'Blank')
+
+
+class Colors:
+    """Enumeration to indicate color of cells on the chart"""
+    GREEN = 0
+    GOLD = 1
+    BLUE = 2
+    PINK = 3
+    WHITE = 4
+    BLANK = 5
 
 
 class LottoDraw(object):
@@ -25,13 +39,12 @@ class LottoDraw(object):
     """
     @staticmethod
     def from_csv(fname):
-        """Return a list of lotto draws as imported from the named file
+        """Generate lotto draws imported from the named file
 
         fname: local file name
         * Assumes the first row (header) can be discarded
         * Discards draws older than OLDEST_DRAW
         """
-        draws = list()
         csvfile = open(fname, 'r', newline='')
         reader = csv.reader(csvfile)
         next(reader)    # discard the first line
@@ -49,9 +62,8 @@ class LottoDraw(object):
                         # we've run out of numbers
                         break
                 # save this draw
-                draws.append(LottoDraw(date_obj, numbers))
+                yield LottoDraw(date_obj, numbers)
         csvfile.close()
-        return draws
 
     def __init__(self, date, numbers):
         """the date of the draw and a list of numbers drawn"""
@@ -66,93 +78,113 @@ class LottoDraw(object):
         return "{} {}".format(self.date, self.numbers)
 
 
-class TextChart(object):
+class LottoChart(object):
+    """
+    A sequence of lottodraws with metadata
+    """
+    class ColorMap(object):
+        def __init__(self):
+            # the current draw and three before
+            self._this = self._last = self._twoback = self._threeback = None
+
+        def is_gold(self, ball):
+            return ball in self._this and ball in self._last
+
+        def is_green(self, ball):
+            return ball in self._this and ball in self._last and ball in self._twoback
+
+        def is_blue(self, ball):
+            return ball in self._this and ball not in self._last and ball in self._twoback
+
+        def is_pink(self, ball):
+            return ball in self._this and ball not in self._last and \
+                   ball not in self._twoback and ball in self._threeback
+
+        def get_color(self, ball):
+            """Returns the constant for the mark of this ball in this draw"""
+            if ball not in self._this:
+                return Colors.BLANK
+            try:
+                if self.is_green(ball):
+                    return Colors.GREEN
+                if self.is_gold(ball):
+                    return Colors.GOLD
+                if self.is_blue(ball):
+                    return Colors.BLUE
+                if self.is_pink(ball):
+                    return Colors.PINK
+            except TypeError:
+                pass
+            return Colors.WHITE
+
+        def update(self, draw):
+            """Return a list of colors for every ball in the draw"""
+
+            # push numbers onto the stack
+            self._threeback = self._twoback
+            self._twoback = self._last
+            self._last = self._this
+            self._this = draw.numbers
+
+            # a list of colors; one for every ball in the draw
+            columns = []
+            for ball in BALLS:
+                columns.append(self.get_color(ball))
+            return columns
+
+    def __init__(self, draws):
+        """create a chart from a list of draws"""
+
+        # generate the rows
+        self.rows = list()
+        colormap = self.ColorMap()
+        for draw in draws:
+            self.rows.append({
+                'date': draw.date,
+                'name': LOTTO_NAME_MAP[draw.date.weekday()],
+                'colors': colormap.update(draw)
+            })
+
+        # tally the frequency of each color of each ball
+        self.tallies = {k: [0]*MAX_BALLS for k in TALLY_NAMES}
+        for row in self.rows:
+            for ball in range(MAX_BALLS):
+                color = row['colors'][ball]
+                tally = TALLY_NAMES[color]
+                self.tallies[tally][ball] += 1
+
+
+class TextWriter(object):
     """
     An ascii rendering of lotto data
     """
-    def __init__(self):
-        self.colormap = ColorMap()
+    def __init__(self, chart):
+        self.chart = chart
 
     @staticmethod
     def _header():
         """return a header showing numbered columns"""
-        numbers = '|'.join(['{:>3}'.format(idx) for idx in range(1, MAX_BALLS+1)])
+        numbers = '|'.join(['{:>3}'.format(idx) for idx in BALLS])
         return "{:^10} {:^15} |{}|\n".format("Date", "Game", numbers)
 
-    @staticmethod
-    def _footer():
+    def _footer(self):
         """return a footer showing numbered columns"""
-        numbers = '|'.join(['{:>3}'.format(idx) for idx in range(1, MAX_BALLS+1)])
-        return "{:^10} {:^15} |{}|\n".format("", "", numbers)
-
-    @staticmethod
-    def _label(drawdate):
-        """return the name of the lotto done on that day"""
-        return ('Monday', 'OzLotto', 'Wednesday', 'Thu', 'Fri', 'TattsLotto', 'Sun')[drawdate.weekday()]
-
-    def render(self, draws):
-        """return a string representation of the lotto data"""
-        string = TextChart._header()
-        for draw in draws:
-            label = TextChart._label(draw.date)
-            columns = ' | '.join(self.colormap.update(draw))
-            string += "{} {:^15} | {} |\n".format(draw.date, label, columns)
-        string += TextChart._footer()
+        numbers = '|'.join(['{:>3}'.format(idx) for idx in BALLS])
+        string = "{:^10} {:^15} |{}|\n\n".format("", "", numbers)
+        for mark in TALLY_NAMES:
+            tally = self.chart.tallies[mark]
+            numbers = '|'.join(['{:>3}'.format(ball) for ball in tally])
+            string += "{:10} {:^15} |{}|\n".format('', mark, numbers)
         return string
 
-
-class ColorMap(object):
-    """
-    Returns colors/marks for each number in a draw
-    """
-    GREEN, GOLD, BLUE, PINK, DRAWN, BLANK = 'C', 'G', 'B', 'P', '*', ' '
-
-    def __init__(self):
-        # the current draw and three before
-        self._this = self._last = self._twoback = self._threeback = None
-
-    def is_gold(self, ball):
-        return ball in self._this and ball in self._last
-
-    def is_green(self, ball):
-        return ball in self._this and ball in self._last and ball in self._twoback
-
-    def is_blue(self, ball):
-        return ball in self._this and ball not in self._last and ball in self._twoback
-
-    def is_pink(self, ball):
-        return ball in self._this and ball not in self._last and \
-               ball not in self._twoback and ball in self._threeback
-
-    def get_color(self, ball):
-        """Returns the constant for the mark of this ball in this draw"""
-        if ball not in self._this:
-            return self.BLANK
-        try:
-            if self.is_green(ball):
-                return self.GREEN
-            if self.is_gold(ball):
-                return self.GOLD
-            if self.is_blue(ball):
-                return self.BLUE
-            if self.is_pink(ball):
-                return self.PINK
-        except TypeError:
-            pass
-        return self.DRAWN
-
-    def update(self, draw):
-        """Returns an array of color marks. Pushes draw onto the stack."""
-        self._threeback = self._twoback
-        self._twoback = self._last
-        self._last = self._this
-        self._this = draw.numbers
-
-        # a list of colors; one for every ball in the draw
-        columns = []
-        for ball in range(1, MAX_BALLS+1):
-            columns.append(self.get_color(ball))
-        return columns
+    def __str__(self):
+        """return a string representation of the lotto data"""
+        string = TextWriter._header()
+        for row in self.chart.rows:
+            columns = ' | '.join(map(('C', 'G', 'B', 'P', '*', ' ').__getitem__, row['colors']))
+            string += "{} {:^15} | {} |\n".format(row['date'], row['name'], columns)
+        string += self._footer()
+        return string
 
 
 # parse commandline arguments
@@ -171,9 +203,9 @@ if args.download:
 
 if __name__ == '__main__':
     # load lotto data
-    ozlotto = LottoDraw.from_csv(OZ_FILENAME)
-    tattslotto = LottoDraw.from_csv(TATTS_FILENAME)
-    weeklotto = LottoDraw.from_csv(WEEK_FILENAME)
+    ozlotto = list(LottoDraw.from_csv(OZ_FILENAME))
+    tattslotto = list(LottoDraw.from_csv(TATTS_FILENAME))
+    weeklotto = list(LottoDraw.from_csv(WEEK_FILENAME))
 
     # put all the draws together
     draws = ozlotto + tattslotto + weeklotto
@@ -181,5 +213,5 @@ if __name__ == '__main__':
 
     # filter for specific days
     mondays = [draw for draw in draws if draw.date.weekday() == 2]
-    chart = TextChart()
-    print(chart.render(draws))
+    chart = LottoChart(mondays)
+    print(TextWriter(chart))
